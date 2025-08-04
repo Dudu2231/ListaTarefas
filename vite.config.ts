09fd1399c87ede00dd443ee06f5b5c5127ccb01b
Dudu2231 <eduardonascimento2231@gmail.com>
2025-08-04 16:49:49 -0300

    fix: tentando rebuildar

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const repoName = 'ListaTarefas'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: `/${repoName}/`
+  base: `/ListaTarefas/`
 })
